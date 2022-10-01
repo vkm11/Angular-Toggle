a6a7b189e2d9cc35cc902c9de175bca4b3b9207a
Vijaykumar Mane <99638967+vkm11@users.noreply.github.com>
2022-10-01 12:43:12 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,18 @@ export class AppComponent {
   title = 'Toggle';
 
   hide:boolean = true;
-  // func(){
-  //   this.hide = !this.hide;
-  // }
+  func(){
+    this.hide = !this.hide;
+  }
 
  // OR
 
-  func(){
-    if(this.hide === false){
-      this.hide =true;
-    }
-    else{
-      this.hide =false
-    }
-  }
+//   func(){
+//     if(this.hide === false){
+//       this.hide =true;
+//     }
+//     else{
+//       this.hide =false
+//     }
+//   }
 }
